83754532f78d385c77087e882a8d28faf459f368
mhmdrz <smrm1381@yahoo.com>
2022-10-31 19:53:53 +0330

    Randomize variables method

--- SAT_model.py
+++ SAT_model.py
@@ -19,3 +19,11 @@ class SAT:
             for i in range(self.clause_number):
                 for term in map(int, fd.readline().split()[:-1]):
                     self.clauses[abs(term), i] = term//abs(term)
+
+        self.randomize_variables()
+
+    def randomize_variables(self) -> None:
+        self.variables = np.ones((1, self.variable_number+1), dtype=int)
+        negs = np.random.choice(self.variable_number, size=np.random.random_integers(
+            self.variable_number), replace=False)
+        self.variables[:, negs] = -1
--- stochastic_hill_climbing.py
+++ stochastic_hill_climbing.py
@@ -2,8 +2,4 @@ from SAT_model import SAT
 
 
 mySat = SAT()
-mySat.read_input('Max-Sat_20_80.txt')
-
-print(mySat.clause_number)
-print(mySat.variable_number)
-print(mySat.clauses)
+mySat.initialize_model('Max-Sat_20_80.txt')
