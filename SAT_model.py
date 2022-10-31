d1dbcd8d009b0251cc1c75301a069d500d978709
mhmdrz <smrm1381@yahoo.com>
2022-10-31 20:31:46 +0330

    Calculate SAT clauses method

--- SAT_model.py
+++ SAT_model.py
@@ -4,10 +4,12 @@ import numpy as np
 class SAT:
     def __init__(self) -> None:
         self.var_num: int
-        self.clause_num: int
         self.vars: np.ndarray
+        self.clause_num: int
         self.clauses: np.ndarray
         self.clause_var_num: np.ndarray
+        self.sat_clauses_num: int
+        self.sat_clauses: list
 
     def initialize_model(self, input_file_name: str) -> None:
         with open(input_file_name) as fd:
@@ -17,17 +19,29 @@ class SAT:
             self.clauses = np.zeros(
                 (self.var_num+1, self.clause_num), dtype=int)
 
-            self.clause_var_num = np.zeros((1, self.clause_num), dtype=int)
+            self.clause_var_num = np.zeros(self.clause_num, dtype=int)
 
             for i in range(self.clause_num):
                 for term in map(int, fd.readline().split()[:-1]):
                     self.clauses[abs(term), i] = term//abs(term)
-                    self.clause_var_num[:, i] += 1
+                    self.clause_var_num[i] += 1
 
+        self.sat_clauses = self.clause_num * [False]
         self.randomize_variables()
 
     def randomize_variables(self) -> None:
-        self.vars = np.ones((1, self.var_num+1), dtype=int)
+        self.vars = np.ones(self.var_num+1, dtype=int)
         negs = np.random.choice(self.var_num, size=np.random.random_integers(
             self.var_num), replace=False)
-        self.vars[:, negs] = -1
+        self.vars[negs] = -1
+
+    def calc_sat(self) -> int:
+        tmp = np.dot(self.vars.reshape(1, -1), self.clauses)
+        tmp += self.clause_var_num.reshape(1, -1)
+
+        self.sat_clauses_num = 0
+        for i, element in enumerate(tmp.reshape(-1)):
+            self.sat_clauses[i] = (element != 0)
+            self.sat_clauses_num += (element != 0)
+
+        return self.sat_clauses_num
--- stochastic_hill_climbing.py
+++ stochastic_hill_climbing.py
@@ -3,3 +3,4 @@ from SAT_model import SAT
 
 mySat = SAT()
 mySat.initialize_model('Max-Sat_20_80.txt')
+print(mySat.calc_sat())
