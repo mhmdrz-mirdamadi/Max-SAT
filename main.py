707d687924ab38a66fc20315b62c9d2fa7d25577
mhmdrz <smrm1381@yahoo.com>
2022-11-03 01:19:48 +0330

    Simulated annealing

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from SAT_model import SAT
-from solver import stochastic_hill_climbing
+from solver import simulated_annealing, stochastic_hill_climbing
 
 
 if __name__ == '__main__':
     mySat = SAT()
     mySat.initialize_model('tests/Max-Sat_20_80.txt')
-    res = stochastic_hill_climbing(mySat, verbose=True)
+    # res = stochastic_hill_climbing(mySat, verbose=True, plot=True)
+    res = simulated_annealing(mySat, verbose=True, plot=True)
     [print(f'{key}: {value}') for key, value in res.items()]
--- solver.py
+++ solver.py
@@ -13,7 +13,8 @@ def return_dict(model: SAT) -> dict:
     }
 
 
-def stochastic_hill_climbing(model: SAT, max_iterations=10000, verbose=False, plot=False) -> dict:
+def stochastic_hill_climbing(model: SAT, max_iterations=10000,
+                             verbose=False, plot=False) -> dict:
     probs = np.zeros(model.var_num+1, dtype=float)
     current_sat = model.calc_sat()
     best_so_far = return_dict(model)
@@ -61,6 +62,72 @@ def stochastic_hill_climbing(model: SAT, max_iterations=10000, verbose=False, pl
         plt.figure(figsize=(10, 6))
         plt.plot(range(max_iterations), history, c='red', linewidth=0.5)
         plt.xlabel('Iteration')
+        plt.ylabel('SAT Clauses')
+        plt.show()
+
+    return best_so_far
+
+
+def simulated_annealing(model: SAT, start_temp=10, stop_temp=0.01,
+                        annealing_schedule=0.9999, verbose=False, plot=False) -> dict:
+    current_sat = model.calc_sat()
+    best_so_far = return_dict(model)
+    current_temp = start_temp
+
+    if verbose:
+        log_str = ''
+
+    if plot:
+        sat_history = []
+        temp_history = []
+
+    while current_temp > stop_temp:
+        if verbose:
+            log_str += f'Temperature: {current_temp:<8.5f}   '
+            log_str += f'SAT: {current_sat:<4}   '
+
+        if plot:
+            sat_history.append(current_sat)
+            temp_history.append(current_temp)
+
+        if current_sat == model.clause_num:
+            break
+
+        index = np.random.choice(range(1, model.var_num+1))
+        next_sat = model.neighbors()[index]
+        delta_e = model.calc_sat(next_sat) - current_sat
+
+        if verbose:
+            log_str += f'Delta E: {delta_e:>2}   Probability: '
+
+        if delta_e > 0:
+            prob = 1.0
+        else:
+            prob = np.exp(delta_e/current_temp)
+
+        choose = np.random.choice([True, False], p=[prob, 1-prob])
+
+        if verbose:
+            log_str += f'{prob:<4.2f}   Chosen: {choose}\n'
+
+        if choose:
+            model.vars = next_sat
+            current_sat += delta_e
+            best_so_far = return_dict(model)
+        else:
+            model.calc_sat()
+
+        current_temp *= annealing_schedule
+
+    if verbose:
+        Path('logs').mkdir(parents=True, exist_ok=True)
+        with open(Path(f'logs/log{len(listdir("logs"))}.log'), 'w') as log:
+            log.write(log_str)
+
+    if plot:
+        plt.figure(figsize=(10, 6))
+        plt.plot(temp_history, sat_history, c='red', linewidth=0.5)
+        plt.xlabel('Temperature')
         plt.ylabel('SAT Clauses')
         plt.show()
 
